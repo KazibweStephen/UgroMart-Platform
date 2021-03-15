5950f9d4bf3c1041cf5357699e9ed3c022e458b1
kstephen3@gmail.com <kstephen3@gmail.com>
2021-03-15 16:42:24 +0300

    initial commit for entire platform combined

---
+++ deliveryms/src/main/java/com/ugromart/deliveryms/DeliverymsApplication.java
@@ -0,0 +1,13 @@
+package com.ugromart.deliveryms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliverymsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliverymsApplication.class, args);
+	}
+
+}
